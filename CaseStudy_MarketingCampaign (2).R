e957af0b09b8286c403db40841f3285770ec474e
iamsmnt <somnathmahato723@gmail.com>
2018-06-14 13:10:22 +0530

    ##Cleaning Environment and loading the data

--- CaseStudy_MarketingCampaign (2).R
+++ CaseStudy_MarketingCampaign (2).R
@@ -1,11 +1,7 @@
-rm(list=ls(all=T))
-setwd("E:/Others/Edwisor/ContentRevamp/MarketingCampaign")
-
 #Load Libraries
 x = c("ggplot2", "corrgram", "DMwR", "caret", "randomForest", "unbalanced", "C50", "dummies", "e1071", "Information",
      "MASS", "rpart", "gbm", "ROSE", 'sampling', 'DataCombine', 'inTrees')
 
-#install.packages(x)
 lapply(x, require, character.only = TRUE)
 rm(x)
 
